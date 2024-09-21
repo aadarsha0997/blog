80dad740837d6783cd7d9ecc4c2acb9734f3ecc2
Ayush <aadarsha0997@gmail.com>
2024-09-21 16:21:55 +0545

    Addin email and tags in post from database

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post,Author,Tag
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=("first_name","email",)
+class TagAdmin(admin.ModelAdmin):
+    list_display=("caption",)
+class PostAdmin(admin.ModelAdmin):
+    list_display=("title",)
+    list_filter=("date","author",)
+
 
 # Register your models here.
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(Post,PostAdmin)
+admin.site.register(Tag,TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    caption=models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.caption}"
+
+class Author(models.Model):
+    first_name=models.CharField( max_length=50)
+    last_name= models.CharField(max_length=50)
+    email  =  models.EmailField( max_length=254)
+    def __str__(self):
+        return f"{self.first_name}"
+             
+
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    slug=models.SlugField(default="",null=True,unique=True)
+    date =models.DateField()
+    img=models.CharField(null=True,max_length=50)
+    excerpt=models.CharField(default="",null=True,max_length=300)
+    content=models.TextField(1000)
+    author=models.ForeignKey(Author,default="unknown",on_delete=models.SET_DEFAULT)
+    tag=models.ManyToManyField(Tag)
+    def __str__(self):
+        return f"{self.title}"
